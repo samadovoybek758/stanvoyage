61e37040897aaf1fb9ef9f2cc962762d982a6a77
muhammadyusufbrr <muhammadyusufdeveloper@gmail.com>
2025-03-06 09:51:30 +0500

    company haqida responsiweligi

--- src/app/[locale]/home/components/CompanyOverview.tsx
+++ src/app/[locale]/home/components/CompanyOverview.tsx
@@ -24,17 +24,17 @@ const CompanyOverview = () => {
   return (
     <section className="mb-[120px]">
       <div className="container">
-        <div className="flex items-center gap-x-10 company-overview-bg pl-10 py-10 pr-[70px] rounded-lg bg-white">
-          <div className="flex-1">
+        <div className="flex items-center gap-x-10 lg:flex-row flex-col gap-5 md:gap-10 company-overview-bg xl:pl-10 xl:py-10 p-5 sm:p-8  xl:pr-[70px] rounded-lg bg-white">
+          <div className="flex-1 w-full">
             <Image
               src={companyIcon}
               alt="company-icon"
               width={100}
               height={100}
-              className="w-16 h-16 mb-24"
+              className="w-16 h-16 xl:mb-24 mb-10"
             />
-            <div className="max-w-[384px]">
-              <h2 className="font-normal text-[32px] leading-[44.8px] text-[#080808] font-brigends-expanded mb-5">
+            <div className="max-w-full lg:max-w-[384px]">
+              <h2 className="font-normal text-2xl md:text-[32px] md:leading-[44.8px] text-[#080808] font-brigends-expanded mb-3 md:mb-5">
                 {isLoading || isFetching ? (
                   <>
                     <Loading className="w-full h-10" />
@@ -48,7 +48,7 @@ const CompanyOverview = () => {
               </h2>
 
               <div
-                className="text-[#080808] font-normal text-lg mb-8"
+                className="text-[#080808] font-normal text-sm sm:text-base md:text-lg mb-4 sm:mb-6 md:mb-8"
                 dangerouslySetInnerHTML={{
                   __html: DOMPurify.sanitize(
                     cleanDescription(
@@ -59,7 +59,7 @@ const CompanyOverview = () => {
               />
               <div>
                 <Button
-                  className="px-11"
+                  className="xl:px-11 px-8 "
                   type="button"
                   text={t("button")}
                   onClick={() => navigation.push(`/${locale}/company`)}
@@ -67,7 +67,7 @@ const CompanyOverview = () => {
               </div>
             </div>
           </div>
-          <div className="h-[456px] w-[640px] overflow-hidden rounded-lg">
+          <div className="flex-1  xl:h-[456px] w-full lg:w-[640px] overflow-hidden rounded-lg">
             {isLoading || isFetching ? (
               <ImageLoading />
             ) : (
